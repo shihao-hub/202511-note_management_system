9c2b3db97428a945c047b57b83a7cf2298afbe4a
shihao-hub <2958017271@qq.com>
2025-11-16 21:05:18 +0800

    待办事项初探索，计划使用单文件嵌入 vue 的方式实现。

--- pages/todolist.py
+++ pages/todolist.py
@@ -1,4 +1,13 @@
-from nicegui import ui
+from nicegui import ui, app
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
+
+from settings import VUE_COMPATIBLE_ENV
+
+
+@app.get("/todolist/index", response_class=HTMLResponse)
+async def todolist_index(request: Request):
+    return VUE_COMPATIBLE_ENV.get_template("todolist_index.html").render({"jinja2": "jinja2-test"})
 
 
 @ui.page("/todolist", title="待办事项")
