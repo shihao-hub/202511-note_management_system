34142186a633b11ec8c8a8149a7be2fc70b767fb
shihao-hub <2958017271@qq.com>
2025-12-07 00:31:22 +0800

    init three

--- dev.py
+++ dev.py
@@ -1,7 +1,7 @@
 import sys, os
 os.chdir(os.path.dirname(__file__))
-sys.path.append(os.path.abspath('app'))
-sys.path.append(os.path.abspath('app.egg'))
-os.chdir(os.path.join(os.path.dirname(__file__), "app"))
+sys.path.append(os.path.abspath('unit'))
+sys.path.append(os.path.abspath('unit.egg'))
+os.chdir(os.path.join(os.path.dirname(__file__), "unit"))
 import main
 main.main()
